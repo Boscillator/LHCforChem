68cfac78f97faf7d19d067af762d51a3f23f2a70
Fred Buchanan <oscillator.b@gmail.com>
2015-07-02 10:07:32 -0400

    Started work on XML

---
+++ Server/loadXml.py
@@ -0,0 +1,13 @@
+import xml.etree.cElementTree as et
+
+class xmlBase(object):
+    pass
+
+class resource(xmlBase):
+    Href = ""
+    Type = ""
+    Name = ""
+    Discription = ""
+
+def loadXML(filename):
+    pass
