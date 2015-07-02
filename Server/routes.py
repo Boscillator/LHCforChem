0a17a38344c803617d09257153210aac35e547f3
Fred Buchanan <oscillator.b@gmail.com>
2015-07-02 11:54:16 -0400

    Added Sections. :)

--- Server/routes.py
+++ Server/routes.py
@@ -8,3 +8,8 @@ from flask import render_template
 @app.route('/')
 def home():
     return render_template('home.html', sections = app.config["models"])
+
+@app.route('/section/<int:id>')
+def section(id):
+    s = app.config["models"][id]
+    return render_template('section.html', section = s, id=id)
