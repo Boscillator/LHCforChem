8b4e736df8f74756af338b6af57a837088debf81
Fred Buchanan <oscillator.b@gmail.com>
2015-07-02 11:28:52 -0400

    Set up base template Made home page

--- Server/__init__.py
+++ Server/__init__.py
@@ -2,12 +2,13 @@
 Main file for LHCforChem
 Does inizilization
 """
-
 from flask import Flask
 from loadXml import loadXML
 
 app = Flask(__name__)
 
-f = open("../config.xml")
+f = open("./config.xml")
 app.config["models"] = loadXML(f.read())
 f.close()
+
+import routes
--- Server/routes.py
+++ Server/routes.py
@@ -3,4 +3,8 @@ Contains routes
 """
 
 from Server import app
+from flask import render_template
 
+@app.route('/')
+def home():
+    return render_template('home.html', sections = app.config["models"])
